291e5310f4b5ff29e817ad5b10d11d7873388460
Aziz Muslim <aziez.musleam@gmail.com>
2024-01-02 14:41:34 +0700

    Penambahan setup cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportHeight: 680,
+    defaultCommandTimeout: 3700
   },
 });
