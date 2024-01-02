4dfc2a7d2dea2e5a3955840adfeb4f552b2cc2b5
Aziz Muslim <aziez.musleam@gmail.com>
2024-01-03 01:22:55 +0700

    Update chooseProduct.cy.js

--- cypress/e2e/3-web-test/LumaShop/chooseProducts.cy.js
+++ cypress/e2e/3-web-test/LumaShop/chooseProducts.cy.js
@@ -8,6 +8,17 @@ describe('My Account -> Edit Address', () => {
         cy.get('#send2').click()
 
         // Tambahkan asersi untuk menunggu halaman Dashboard benar-benar terbuka
-        cy.contains('Welcome').should('be.visible')
+        cy.contains('New Luma Yoga Collection').should('be.visible')
+    })
+
+    it('Shop Products', () => {
+        cy.get('.home-main > .content > .action').click()
+        cy.get(':nth-child(1) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
+        cy.get('#option-label-size-143-item-172').click()
+        cy.get('#option-label-color-93-item-50').click()
+        cy.get('#qty').clear().type('2')
+        cy.get('#product-addtocart-button > span').click()
+        cy.get('.showcart').click()
+        
     })
 })
