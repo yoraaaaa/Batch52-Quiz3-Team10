f2aecf5953087423eec1d6ed42832efe8f004ae3
Aziz Muslim <aziez.musleam@gmail.com>
2024-01-03 10:35:28 +0700

    update baseLumaShop.page.js

--- cypress/support/pageObject/lumaShop/baseLumaShop.page.js
+++ cypress/support/pageObject/lumaShop/baseLumaShop.page.js
@@ -1,27 +1,4 @@
-class Login {
-    visitHomePage() {
-        cy.visit('https://magento.softwaretestingboard.com/')
-        cy.get('.panel > .header > .authorization-link > a').click()
-        cy.get('#email').type('adrianoqalbi@gmail.com')
-        cy.get('#pass').type('adrianoQalbi1')
-        cy.get('#send2').click()
-    }
-    // Jika ada langkah-langkah lain yang ingin dimasukkan ke dalam objek ini, Anda dapat menambahkannya di sini
-}
 
-class Shop {
-    shopProducts() {
-        cy.get('.home-main > .content > .action').click()
-        cy.get(':nth-child(1) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
-        cy.get('#option-label-size-143-item-172').click()
-        cy.get('#option-label-color-93-item-50').click()
-        cy.get('#qty').clear().type('2')
-        cy.get('#product-addtocart-button > span').click()
-        cy.get('.showcart').click()
-    }
-
-    // Jika ada langkah-langkah lain yang ingin dimasukkan ke dalam objek ini, Anda dapat menambahkannya di sini
-}
 
 class Visit {
     visitMyAccount(){
@@ -56,8 +33,8 @@ class Visit {
     }
 }
 
-const login = new Login()
-const shop = new Shop()
+
+
 const visit = new Visit()
 
-export { login, shop , visit}
+export { visit}
